a7044d4e1b307ccc49d703b61e8adc904f25a595
Ben Haworth <ben.haworth@contentful.com>
2025-10-24 10:32:29 +0100

    now with test jeyyfish api

---
+++ run_jellyfish_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Wrapper script to run the Jellyfish API test script with proper PYTHONPATH
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export PYTHONPATH="${SCRIPT_DIR}/package:${PYTHONPATH}"
+
+python3 "${SCRIPT_DIR}/test_jellyfish_api.py" "$@"
+
